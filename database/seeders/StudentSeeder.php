78b7c00e2d4a2e83f4434b809f5f2b72b7aa80c2
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2024-01-14 18:02:09 +0700

    ✅ Mengisi Table Database dengan FACTORY (FAKER)

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -15,26 +15,29 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        Schema::disableForeignKeyConstraints();
-        Student::truncate();
-        Schema::enableForeignKeyConstraints();
 
-        $data = [
-            ['name' => 'aiu', 'gender' => 'P', 'nis' => '0101001', 'class_id' => 2],
-            ['name' => 'budi', 'gender' => 'L', 'nis' => '0101002', 'class_id' => 2],
-            ['name' => 'siti', 'gender' => 'P', 'nis' => '0101003', 'class_id' => 1],
-            ['name' => 'tono', 'gender' => 'L', 'nis' => '0101004', 'class_id' => 3],
-        ];
+        Student::factory()->count(20)->create();
 
-        foreach ($data as $value) {
-            Student::insert([
-                'name' => $value['name'],
-                'gender' => $value['gender'],
-                'nis' => $value['nis'],
-                'class_id' => $value['class_id'],
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-        }
+        // Schema::disableForeignKeyConstraints();
+        // Student::truncate();
+        // Schema::enableForeignKeyConstraints();
+
+        // $data = [
+        //     ['name' => 'aiu', 'gender' => 'P', 'nis' => '0101001', 'class_id' => 2],
+        //     ['name' => 'budi', 'gender' => 'L', 'nis' => '0101002', 'class_id' => 2],
+        //     ['name' => 'siti', 'gender' => 'P', 'nis' => '0101003', 'class_id' => 1],
+        //     ['name' => 'tono', 'gender' => 'L', 'nis' => '0101004', 'class_id' => 3],
+        // ];
+
+        // foreach ($data as $value) {
+        //     Student::insert([
+        //         'name' => $value['name'],
+        //         'gender' => $value['gender'],
+        //         'nis' => $value['nis'],
+        //         'class_id' => $value['class_id'],
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now()
+        //     ]);
+        // }
     }
 }
