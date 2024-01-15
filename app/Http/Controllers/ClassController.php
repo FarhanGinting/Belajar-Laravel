4f6897dea7a7bef01bd1b943504dd7e6dd44c3ff
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2024-01-15 20:51:58 +0700

    ✅ N+1 Problem, Lazy Loading vs Eager Loading

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -8,7 +8,22 @@ use Illuminate\Http\Request;
 class ClassController extends Controller
 {
     public function index(){
-        $class = ClassRoom::with('students')->get();
+
+        
+       
+        // 1️⃣ Layz Load 🐌
+        // $class = ClassRoom::all(); // Cara Request Data => Hanya ketika dibutuhkan ambil data
+        // Select * from table class
+        // Select * from student where class = 1A
+        // Select * from student where class = 1B
+        // Select * from student where class = 1C
+        // Select * from student where class = 1D
+
+
+         // 2️⃣Eager Load 👑
+         $class = ClassRoom::with('students')->get(); // Cara Request Data => Mengikuti Kode yang diberikan
+         // Select * from table class
+         // Select * from student where class in (1A,1B,1C,1D)
         return view('classroom', ['classList' => $class]);
     }
 }
