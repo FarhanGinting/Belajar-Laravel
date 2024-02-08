ff8458d4ad15f4657e5942436f8d32d16bef14e3
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2024-02-08 15:20:17 +0700

    ♻️ Restore View

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Teacher;
+
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
     public function index(){
         $teacher = Teacher::all();
-        return view('teacher', ['teacherList' => $teacher]);
+        dd($teacher);
+        // return view('teacher', ['teacherList' => $teacher]);
     }
 
     public function show($id){
